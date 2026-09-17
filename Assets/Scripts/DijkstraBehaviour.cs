d1f17ba01cb53edaf365e475d7beccf55ef3e7a7
Niki D'Arcy <118869960+102106269@users.noreply.github.com>
2026-09-17 22:00:15 +1000

    initial dijkstra script start

---
+++ Assets/Scripts/DijkstraBehaviour.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+
+public class DijkstraBehaviour
+{
+    [SerializeField] private List<GameObject> tree = new List<GameObject>();
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void Dijkstra()
+    {
+
+    }
+    
+}
