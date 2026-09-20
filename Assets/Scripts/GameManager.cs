e287a5194da9e1f7415d0a039b0c258b76f5a067
Niki D'Arcy <118869960+102106269@users.noreply.github.com>
2026-09-20 16:57:01 +1000

    move LoadCameras script logic to GameManager script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -6,4 +8,17 @@ public class GameManager : MonoBehaviour
     public Sprite gridSprite;
     public int cell_width = 10;
     public int cell_height = 10;
+
+    [Header("CRT Camera Scenes")]
+    [SerializeField] private List<string> scenes = new List<string>();
+    void Start()
+    {
+        foreach (var scene in scenes)
+        {
+            if (!SceneManager.GetSceneByName(scene).isLoaded)
+            {
+                SceneManager.LoadSceneAsync(scene, LoadSceneMode.Additive);
+            }
+        }
+    }
 }
